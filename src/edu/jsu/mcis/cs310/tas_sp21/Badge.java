8b6f3fae84fb8b31086e31397949ab6e2bf21f09
Apeksha Dahal <adahal@stu.jsu.edu>
2021-03-01 16:11:43 -0600

    Added Badge class

---
+++ src/edu/jsu/mcis/cs310/tas_sp21/Badge.java
@@ -0,0 +1,29 @@
+ 
+package edu.jsu.mcis.cs310.tas_sp21;
+
+ 
+public class Badge {
+    
+    private String id;
+    private String description;
+
+    public Badge(String id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+   public String toString(){
+       StringBuilder s = new StringBuilder();
+       s.append("#").append(id).append(" (").append(description).append(")");
+       return s.toString();
+       
+   }
+}
