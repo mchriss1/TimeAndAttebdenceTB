79c6d2dec25d1a7ea94135c6c443f8f46430baac
Apeksha Dahal <adahal@stu.jsu.edu>
2021-03-03 00:01:21 -0600

    Added getShift() in TASDatabase class

--- src/edu/jsu/mcis/cs310/tas_sp21/TASDatabase.java
+++ src/edu/jsu/mcis/cs310/tas_sp21/TASDatabase.java
@@ -147,10 +147,141 @@ public TASDatabase(){
         return null;
         
     }
+
+  //GET SHIFT BY ID
+    
+    public Shift getShift (int shiftid){
+        
+        try{
+            
+             
+            pstSelect = conn.prepareStatement("SELECT * FROM shift WHERE id = ?");
+
+             
+            pstSelect.setInt(1, shiftid);
+
+           
+            pstSelect.execute();
+            resultset = pstSelect.getResultSet();
+
+             
+            resultset.first();
+
+            String description = resultset.getString("description");
+
+            String start = resultset.getString("start");
+            String[] arrayStart = start.split(":");
+            int startHour = Integer.parseInt(arrayStart[0]);
+            int startMin = Integer.parseInt(arrayStart[1]);
+
+            String stop = resultset.getString("stop");
+            String[] arrayStop = stop.split(":");
+            int stopHour = Integer.parseInt(arrayStop[0]);
+            int stopMin = Integer.parseInt(arrayStop[1]);
+
+            int interval = resultset.getInt("interval");
+            int gracePeriod = resultset.getInt("graceperiod");
+            int dock = resultset.getInt("dock");
+
+            String lunchStart = resultset.getString("lunchstart");
+            String[] arrayLunchStart = lunchStart.split(":");
+            int lunchStartHour = Integer.parseInt(arrayLunchStart[0]);
+            int lunchStartMin = Integer.parseInt(arrayLunchStart[1]);
+
+            String lunchStop = resultset.getString("lunchstop");
+            String[] arrayLunchStop = lunchStop.split(":");
+            int lunchStopHour = Integer.parseInt(arrayLunchStop[0]);
+            int lunchStopMin = Integer.parseInt(arrayLunchStop[1]);
+
+            int lunchDeduct = resultset.getInt("lunchdeduct");
+
+            Shift s = new Shift(description, startHour, startMin, stopHour, stopMin, gracePeriod, interval,
+                    dock, lunchStartHour, lunchStartMin, lunchStopHour, lunchStopMin, lunchDeduct);
+
+            return s;
+            
+        }
+                
+        catch(Exception e){
+            System.err.println("** getShift: " + e.toString());
+        }
+
+        return null;
+    }
+    
+    public void close() {
+        
+        try{
+            
+            conn.close();
+        }
+        catch(Exception e){
+            System.err.println("** close: " + e.toString());
+        }
+    }
+    
+  //GET SHIFT BY BADGE 
+  public Shift getShift(Badge badge){
+        
+        try{
+            
+            
+            pstSelect = conn.prepareStatement("SELECT * FROM shift INNER JOIN employee"
+                    + " ON employee.shiftid = shift.id INNER JOIN badge ON badge.id"
+                    + " = employee.badgeid WHERE badge.id = ?");
+
+           
+            pstSelect.setString(1, badge.getId() );
+
+             
+            pstSelect.execute();
+            resultset = pstSelect.getResultSet();
+
+             
+            resultset.first();
+
+            String description = resultset.getString("description");
+
+            String start = resultset.getString("start");
+            String[] arrayStart = start.split(":");
+            int startHour = Integer.parseInt(arrayStart[0]);
+            int startMin = Integer.parseInt(arrayStart[1]);
+
+            String stop = resultset.getString("stop");
+            String[] arrayStop = stop.split(":");
+            int stopHour = Integer.parseInt(arrayStop[0]);
+            int stopMin = Integer.parseInt(arrayStop[1]);
+
+            int interval = resultset.getInt("interval");
+            int gracePeriod = resultset.getInt("graceperiod");
+            int dock = resultset.getInt("dock");
+
+            String lunchStart = resultset.getString("lunchstart");
+            String[] arrayLunchStart = lunchStart.split(":");
+            int lunchStartHour = Integer.parseInt(arrayLunchStart[0]);
+            int lunchStartMin = Integer.parseInt(arrayLunchStart[1]);
+
+            String lunchStop = resultset.getString("lunchstop");
+            String[] arrayLunchStop = lunchStop.split(":");
+            int lunchStopHour = Integer.parseInt(arrayLunchStop[0]);
+            int lunchStopMin = Integer.parseInt(arrayLunchStop[1]);
+
+            int lunchDeduct = resultset.getInt("lunchdeduct");
+
+            Shift s = new Shift(description, startHour, startMin, stopHour, stopMin, gracePeriod, interval,
+                    dock, lunchStartHour, lunchStartMin, lunchStopHour, lunchStopMin, lunchDeduct);
+
+            return s;
+        }
+        
+        catch(Exception e){
+            System.err.println("** getShift: " + e.toString());
+        }
+
+        return null;
+    }   
 }
 
-    
-    
      
   
     
