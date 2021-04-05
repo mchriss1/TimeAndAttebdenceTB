a8cccc8cc2be3fdfdf10f4a7cdb36fad15b02986
Santoshi Giri <sgiri@stu.jsu.edu>
2021-04-05 16:01:46 -0500

    Started to work on F4:TASLogic.java()

--- src/edu/jsu/mcis/cs310/tas_sp21/TASLogic.java
+++ src/edu/jsu/mcis/cs310/tas_sp21/TASLogic.java
@@ -3,5 +3,31 @@ package edu.jsu.mcis.cs310.tas_sp21;
 
  
 public class TASLogic {
+   
+    public static final int CLOCKIN = 1;
+ 
+    public static final int CLOCKOUT = 0;
     
+ 
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, 
+            Shift shift) {
+
+        int totalMin = 0;
+        long totalMillis = 0;
+        long inTime = 0;
+        long outTime = 0;
+        int punchCounter = 0;
+        
+        int lunchTime = 30;
+        
+        for(int i = 0; i < dailypunchlist.size(); i++) {
+            
+           if (dailypunchlist.get(i).getPunchtypeid() == CLOCKIN) 
+           {
+               
+               inTime = dailypunchlist.get(i).getAdjustedTimeStamp() ;
+               punchCounter++;
+               continue;
+               
+           }
 }
