8f36dd0853ea9cc6da9e32d4ecade74a71a35f9d
Santoshi Giri <sgiri@stu.jsu.edu>
2021-04-14 19:28:19 -0500

    Feature 4 completed  and Test were passed

--- src/edu/jsu/mcis/cs310/tas_sp21/TASLogic.java
+++ src/edu/jsu/mcis/cs310/tas_sp21/TASLogic.java
@@ -1,11 +1,11 @@
- 
 package edu.jsu.mcis.cs310.tas_sp21;
+
+import java.util.ArrayList;
 
  
 public class TASLogic {
-   
+    
     public static final int CLOCKIN = 1;
- 
     public static final int CLOCKOUT = 0;
     
  
@@ -17,17 +17,101 @@ public class TASLogic {
         long inTime = 0;
         long outTime = 0;
         int punchCounter = 0;
-        
         int lunchTime = 30;
         
         for(int i = 0; i < dailypunchlist.size(); i++) {
             
-           if (dailypunchlist.get(i).getPunchtypeid() == CLOCKIN) 
-           {
+           if (dailypunchlist.get(i).getPunchtypeid() == CLOCKIN) {
                
-               inTime = dailypunchlist.get(i).getAdjustedTimeStamp() ;
+               inTime = dailypunchlist.get(i).getAdjustedTimeStamp().getTime();
                punchCounter++;
                continue;
                
            }
-}
+
+           if (dailypunchlist.get(i).getPunchtypeid() == CLOCKOUT) {
+               
+               outTime = dailypunchlist.get(i).getAdjustedTimeStamp().getTime();
+               punchCounter++;
+               
+           }
+           
+           if (inTime != 0 && outTime != 0) {
+               
+               totalMillis += outTime - inTime;
+               
+           }
+           
+           
+           inTime = 0;
+           outTime = 0;
+           
+        }
+        
+        if (totalMillis != 0) {
+            
+            totalMin = (int) (totalMillis/60000);
+            
+        }
+        
+        if (totalMin > shift.getShiftLunchDeduct() && punchCounter <= 3) {
+            
+            totalMin -= lunchTime;
+            
+        }
+        
+        return totalMin;
+        
+    }    
+   
+     
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
